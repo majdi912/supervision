139bd6d8b4cceecc019f5c5718da554330a07392
majdi <majdislimen14@gmail.com>
2021-07-19 11:25:00 +0100

    delete for docker container

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,9 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'NAME': 'sqlite3',
+
+      
     }
 }
 
