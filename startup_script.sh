1a6c99b98ff6ffc123de30e0bbfa2d4cb26f72fd
majdi912 <61276874+majdi912@users.noreply.github.com>
2022-05-09 09:19:41 +0100

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,5 +1,8 @@
 sudo usermod -aG docker $USER
 newgrp docker
-printf 'yes' | gcloud auth configure-docker
+sudo curl -L https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 curl https://raw.githubusercontent.com/majdi912/supervision/main/docker-compose.yml --output docker-compose.yml
-docker-compose up 
+printf "yes" | gcloud auth configure-docker
+docker-compose up
